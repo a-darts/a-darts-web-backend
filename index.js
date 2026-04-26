8466c47fe6600c812eb0897e8dcad85a9e40ef9c
arianaporroche <874055@unizar.es>
2026-04-26 19:22:27 +0200

    Initial commit

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    console.log(`🔧 Environment: ${process.env.NODE_ENV || 'development'}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
